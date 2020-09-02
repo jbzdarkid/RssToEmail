cc02aea217a03ecb3ad0811ca5daf884ede2f2b7
jbzdarkid <jbzdarkid@gmail.com>
2020-09-01 20:06:56 -0700

    Change secret names, rm datetime

--- rss_to_email.py
+++ rss_to_email.py
@@ -3,11 +3,20 @@ from email.message import EmailMessage
 from email.mime.text import MIMEText
 import feedparser
 from os import environ
-from datetime import datetime
-from time import mktime
+from time import mktime, time
 from json import load
 
-def to_datetime(struct_time):
+try:
+    SENDER_EMAIL = environ['sender_email'].decode('utf-8')
+    SENDER_PWORD = environ['sender_pword'].decode('utf-8')
+    TARGET_EMAIL = environ['target_email'].decode('utf-8')
+except KeyError:
+    SENDER_EMAIL = None
+    SENDER_PWORD = None
+    TARGET_EMAIL = None
+
+
+def to_seconds(struct_time):
     if struct_time is None:
         return None
     return int(mktime(struct_time))
@@ -18,24 +27,26 @@ def parse_feeds(cache, feed_url, email_server):
     feed_title = d['feed']['title']
 
     # Not present for all feeds
-    feed_updated = to_datetime(d['feed'].get('published_parsed') or d.get('updated_parsed'))
+    feed_updated = to_seconds(d['feed'].get('published_parsed') or d.get('updated_parsed'))
     if feed_updated < cache['last_updated']:
         return # No need to parse entries if the feed hasn't been updated since then
 
     if feed_url not in cache:
         cache[feed_url] = {
             'name': feed_title,
+            'last_updated': 0,
             'seen_entries': [],
         }
     else:
         # Potentially update title
         cache[feed_url]['name'] = feed_title
+        cache[feed_url]['last_updated'] = int(time.time())
 
     for entry in d['entries']:
         title = entry['title']
         link = entry['link']
         # Not all entries have a date
-        entry_date = to_datetime(entry.get('published_parsed'))
+        entry_date = to_seconds(entry.get('published_parsed'))
         content = None
         if 'content' not in entry:
             content = entry['description']
@@ -61,9 +72,9 @@ def send_email(email_server, title, content):
 
     msg = EmailMessage()
     msg['Subject'] = title
-    msg['To'] = environ['target_email']
-    msg['From'] = environ['sender_email']
-    msg['Date'] = datetime.today().strftime('%A, %B %d, %Y') 
+    msg['To'] = TARGET_EMAIL
+    msg['From'] = SENDER_EMAIL
+    msg['Date'] = time.time().strftime('%A, %B %d, %Y') 
 
     msg.attach(MIMEText(content, 'html'))
     email_server.sendmail(msg['From'], msg['To'], msg.as_string())
@@ -76,8 +87,8 @@ if __name__ == '__main__':
     email_server = SMTP('smtp.gmail.com', 587)
     email_server.ehlo()
     email_server.starttls()
-    if 'sender_email' in environ:
-        email_server.login(environ['sender_email'], environ['sender_password'])
+    if SENDER_EMAIL:
+        email_server.login(SENDER_EMAIL, SENDER_PASSWORD);
 
     with open('feed_list.txt', 'r') as f:
         for feed_url in f:
@@ -90,5 +101,3 @@ if __name__ == '__main__':
 
     with open('entries_cache.json', 'r') as f:
         dump(cache, j, sort_keys=True, indent=2)
-
-    # Commit changes... ?
