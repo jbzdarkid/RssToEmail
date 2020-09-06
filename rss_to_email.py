34d3c29d126565240f9e122d158a4503be499d6a
jbzdarkid <jbzdarkid@gmail.com>
2020-09-06 10:41:35 -0700

    Add link to article

--- rss_to_email.py
+++ rss_to_email.py
@@ -27,9 +27,9 @@ def parse_feeds(cache, feed_url, email_server):
     feed_title = d['feed']['title']
 
     # Not present for all feeds
-    feed_updated = to_seconds(d['feed'].get('published_parsed') or d.get('updated_parsed'))
-    if feed_updated < cache['last_updated']:
-        return # No need to parse entries if the feed hasn't been updated since then
+    #feed_updated = to_seconds(d['feed'].get('published_parsed') or d.get('updated_parsed'))
+    #if feed_updated < cache['last_updated']:
+    #    return # No need to parse entries if the feed hasn't been updated since then
 
     if feed_url not in cache:
         cache[feed_url] = {
@@ -59,12 +59,15 @@ def parse_feeds(cache, feed_url, email_server):
         if entry_date:
             if entry_date > cache[feed_url]['last_updated']:
                 send_email(email_server, title, link, content)
+                cache[feed_url]['last_updated'] = entry_date
         else:
             if link not in cache[feed_url]['seen_entries']:
                 send_email(email_server, title, link, content)
                 cache[feed_url]['seen_entries'].append(link)
+
         with open('entries_cache.json', 'w') as f:
             dump(cache, f, sort_keys=True, indent=2)
+        exit(0)
 
 
 def send_email(email_server, title, link, content):
@@ -75,6 +78,7 @@ def send_email(email_server, title, link, content):
     # msg['Date'] = time()
 
     msg.set_content('New RSS post: ' + link)
+    content = f'To view the full post, <a href="{link}">click here</a>.<br>{content}'
     msg.add_alternative(content, subtype='html')
     email_server.sendmail(SENDER_EMAIL, TARGET_EMAIL, msg.as_string())
 
@@ -90,9 +94,10 @@ if __name__ == '__main__':
         email_server.login(SENDER_EMAIL, SENDER_PWORD);
 
     with open('feed_list.txt', 'r') as f:
-        for feed_url in f:
-            if feed_url.startswith('#'):
-                continue
+        for line in f:
+            feed_url = line[:line.find('#')].strip()
+            if feed_url == '':
+              continue
             parse_feeds(cache, feed_url, email_server)
 
     email_server.quit()
