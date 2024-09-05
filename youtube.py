c8f46446bc5beb019abc378647afd6b0738eb714
jbzdarkid <jbzdarkid@gmail.com>
2024-09-04 22:42:18 -0700

    Convert youtube.py to actually use the youtube APIs

--- youtube.py
+++ youtube.py
@@ -1,24 +1,69 @@
+import os
 import requests
 from datetime import datetime, timezone
 
-import generic
+from entry import Entry
+
+API_KEY = os.environ('youtube_token')
 
 def get_entries(cache, feed_url):
-    # Special handling to discard youtube premiers, otherwise identical to the generic RSS handler
-    for entry in generic.get_entries(cache, feed_url):
-        r = requests.get(entry.link)
-        data = r.text
+  if 'channel_id' in feed_url: # By channel
+    channel_id = feed_url.split('channel_id=')[1]
+    upload_playlist = get_channel_upload_playlist(channel_id)
+    videos = get_playlist_items(playlist_id)
+    return get_video_entries(videos)
+  elif 'playlist_id' in feed_url: # By playlist
+    playlist_id = feed_url.split('playlist_id=')[1]
+    videos = get_playlist_items(playlist_id)
+    return get_video_entries(videos)
 
-        # "Live Broadcast" means premier
-        if '<meta itemprop="isLiveBroadcast" content="True">' in data:
-            start_date_idx = data.find('<meta itemprop="startDate" content="')
-            if start_date_idx == -1:
-                continue
-            start_date_str = data[start_date_idx+36:start_date_idx+36+25]
-            start_date = datetime.strptime(start_date_str, '%Y-%m-%dT%H:%M:%S%z')
 
-            if start_date > datetime.now(timezone.utc):
-                continue
+def get_channel_upload_playlist(channel_id):
+  params = {
+    'key': API_KEY,
+    'part': 'contentDetails',
+    'id': channel_id,
+    'maxResults': 50,
+  }
 
-        # Not a youtube premier or the premier has released
-        yield entry
+  r = requests.get('https://www.googleapis.com/youtube/v3/channels', params=params)
+  j = r.json()
+  return j['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+def get_playlist_items(playlist_id):
+  params = {
+    'key': API_KEY,
+    'part': 'contentDetails',
+    'playlistId': playlist_id,
+    'maxResults': 50, # Per page, in playlist order
+  }
+
+  r = requests.get('https://www.googleapis.com/youtube/v3/playlistItems', params=params)
+  j = r.json()
+  return [item['contentDetails']['videoId'] for item in j['items']]
+
+
+def get_video_entries(video_ids):
+  params = {
+    'key': API_KEY,
+    'part': 'id,liveStreamingDetails,snippet',
+    'id': ','.join(video_ids),
+  }
+
+  r = requests.get('https://www.googleapis.com/youtube/v3/videos', params=params)
+  j = r.json()
+  for video in j['items']:
+    if 'liveStreamingDetails' in video:
+      start_time = datetime.strptime(video['liveStreamingDetails']['scheduledStartTime'], '%Y-%m-%dT%H:%M:%SZ')
+      if start_time > datetime.now():
+        print(f'Found YT premier starting in the future, skipping: {start_time} {video["id"]}')
+        continue
+
+    entry = Entry()
+    entry.title = video['snippet']['title']
+    entry.content = video['snippet']['description']
+    entry.link = 'https://www.youtube.com/watch?v=' + video['id']
+    published_at = datetime.strptime(video['snippet']['publishedAt'], '%Y-%m-%dT%H:%M:%SZ')
+    entry.date = int(published_at.timestamp())
+
+    yield entry
