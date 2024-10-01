313a0141ef3bcbfdd686535edade8c2886eb12cf
jbzdarkid <jbzdarkid@gmail.com>
2024-09-30 21:27:22 -0700

    Switching to gmail because outlook MSA auth is hard

---
+++ oauth2.py
@@ -0,0 +1,37 @@
+import os
+import requests
+import smtplib
+
+class EmailServer():
+  def __init__(self):
+    self.username = os.environ.get('sender_email', None)
+    self.client_id = os.environ.get('email_client_id', None)
+    self.client_secret = os.environ.get('email_client_secret', None)
+    self.refresh_token = os.environ.get('email_refresh_token', None)
+
+  def __enter__(self):
+    self.connection = smtplib.SMTP_SSL('smtp.gmail.com', context=ssl.create_default_context())
+    auth_string = self.get_auth_string()
+    # base64.b64encode(auth_string)
+    self.connection.docmd('AUTH', f'XOAUTH2 {auth_string}')
+    return self
+
+  def sendmail(self, from_addr, to_addrs, msg):
+    return self.connection.sendmail(from_addr, to_addrs, msg)
+    
+  def __exit__(self):
+    self.connection.quit()
+
+  def get_auth_string(self):
+    params = {
+      'grant_type': 'refresh_token',
+      'client_id': self.client_id,
+      'client_secret': self.client_secret,
+      'refresh_token': self.refresh_token,
+    }
+
+    r = requests.post('https://accounts.google.com/o/oauth2/token', params=params)
+    r.raise_for_status()
+    access_token = r.json()['access_token']
+
+    return f'user={self.username}\1auth=Bearer {access_token}\1\1'
