5d50ae774a5c72a1035af6141e53177a3d91530e
jbzdarkid <jbzdarkid@gmail.com>
2024-10-08 18:32:50 -0700

    Added notes on how to refresh the token

--- oauth2.py
+++ oauth2.py
@@ -11,8 +11,12 @@ class EmailServer():
     self.client_id = os.environ.get('email_client_id', None)
     self.client_secret = os.environ.get('email_client_secret', None)
     self.refresh_token = os.environ.get('email_refresh_token', None)
+    self.connection = None
 
   def __enter__(self):
+    if not self.refresh_token:
+      return self # Local testing, etc
+
     self.connection = smtplib.SMTP_SSL('smtp.gmail.com', context=ssl.create_default_context())
     self.connection.ehlo()
     auth_string = self.get_auth_string()
@@ -23,7 +27,22 @@ class EmailServer():
     return self.connection.sendmail(from_addr, to_addrs, msg)
     
   def __exit__(self, exc_type, exc_value, traceback):
-    self.connection.quit()
+    if self.connection:
+      self.connection.quit()
+
+  def get_refresh_token(self, client_secret, auth_code):
+    params = {
+      'grant_type': 'authorization_code',
+      'redirect_uri': 'https://oauth2.dance/',
+      'client_id': self.client_id,
+      'client_secret': client_secret,
+      'code': auth_code,
+    }
+
+    r = requests.post('https://accounts.google.com/o/oauth2/token', params=params)
+    if not r.ok:
+      print(r.status_code, r.text)
+    return r.json()['refresh_token']
 
   def get_auth_string(self):
     params = {
@@ -39,3 +58,21 @@ class EmailServer():
     access_token = r.json()['access_token']
 
     return 'user=%s\1auth=Bearer %s\1\1' % (self.username, access_token)
+
+if __name__ == '__main__':
+  with EmailServer() as server:
+    server.client_id = input('Client id: ')
+
+    print('Please visit this URL:')
+    print('https://accounts.google.com/o/oauth2/auth', end='')
+
+    print('?access_type=offline', end='')
+    print('&prompt=consent', end='')
+    print('&response_type=code', end='')
+    print('&redirect_uri=https%3A%2F%2Foauth2.dance%2F', end='')
+    print('&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fgmail.send', end='')
+    print(f'&client_id={server.client_id}')
+
+    auth_code = input('Enter auth code: ')
+    client_secret = input('Client secret: ')
+    print('Refresh token:', server.get_refresh_token(client_secret, auth_code))
