091c987b2d8280fa4e5c023d7ac24268d7b21f75
jbzdarkid <jbzdarkid@gmail.com>
2024-12-08 18:05:34 -0800

    Sort and fix sorting

--- sort_feedlist.py
+++ sort_feedlist.py
@@ -1,25 +1,18 @@
-import feedparser
-
-def get_feed_name(feed_url):
-    d = feedparser.parse(feed_url)
-    return d['feed'].get('title', None)
-
 feeds = []
 with open('feed_list.txt', 'r') as f:
-    for line in f:
-        feed = line[:line.find('#')].strip()
-        if feed == '':
-            continue
-        if feed.startswith('http://'):
-            feed = feed.replace('http', 'https', 1)
-        sortkey = feed[8:]
+    lines = f.read().split('\n')
+    for i in range(0, len(lines)-1, 2):
+        feed_name = lines[i]
+        feed_url = lines[i+1]
+
+        sortkey = feed_url.split('://')[1]
         if sortkey.startswith('www.'):
             sortkey = sortkey[4:]
-        feeds.append((sortkey, feed))
+        feeds.append((sortkey, feed_name, feed_url))
+
 feeds.sort()
+
 with open('feed_list.txt', 'w', encoding='utf-8') as f:
-    for feed in feeds:
-        feed_name = get_feed_name(feed[1])
-        if feed_name:
-            f.write('# ' + get_feed_name(feed[1]) + '\n')
-        f.write(feed[1] + '\n')
+    for _, feed_name, feed_url in feeds:
+        f.write(feed_name + '\n')
+        f.write(feed_url + '\n')
