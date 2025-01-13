822d77b95f2f0f9ac1ebb0f5c5200910ea175970
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2025-01-13 09:38:24 +0000

    Last touches

--- send_tweets.py
+++ send_tweets.py
@@ -24,9 +24,9 @@ if __name__ == '__main__':
   msg['To'] = TARGET_EMAIL
   msg['Reply-To'] = TARGET_EMAIL
   email_body = ''
-  for i, t in enumerate(tweets):
+  for t in tweets:
     email_body += '<hr><br>'
-    email_body += f'Tweet {i+1} sent at ' + strftime("%A, %B %d, %Y", localtime(t.date)) + '\n'
+    email_body += f'<a href="{t.link}">Tweet</a> sent at ' + strftime("%A, %B %d, %Y", localtime(t.date)) + '\n'
     email_body += t.content + '\n'
 
   msg.set_content("Hello, world")
