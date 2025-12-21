6567ead0d3f4ffd2cbc99b2a95a1e981b1aaef72
jbzdarkid <jbzdarkid@gmail.com>
2025-12-20 16:31:44 -0800

    I think this is safe & functional

--- sort_feedlist.py
+++ sort_feedlist.py
@@ -7,28 +7,26 @@ entries_cache = Path('entries_cache.json')
 with entries_cache.open('r', encoding='utf-8') as f:
     entries_cache_json = json.load(f)
 
-    for feed_url in list(entries_cache_json.keys()):
-      data = entries_cache_json[feed_url]
-
 feeds = []
 with feed_list.open('r', encoding='utf-8') as f:
     lines = f.read().split('\n')
     for i in range(0, len(lines)-1, 2):
-        feed_name = lines[i][2:]
+        feed_name = lines[i].lstrip('# ')
         feed_url = lines[i+1]
 
-        if feed_url not in entries_cache_json:
-          print(f'Found newly-added feed {feed_name}: {feed_url}, adding to the cache')
-          entries_cache_json[feed_url] = {
-            'name': feed_name,
-            'last_updated': 0,
-            'seen_entries': [],
-          }
-          continue
-
-        if 'title' in data:
-          data.pop('title')
-        data['name'] = feed_name
+        if not feed_url.startswith('#'):
+          if feed_url not in entries_cache_json:
+            print(f'Found newly-added feed {feed_name}: {feed_url}, adding to the cache')
+            entries_cache_json[feed_url] = {
+              'name': feed_name,
+              'last_updated': 0,
+              'seen_entries': [],
+            }
+          else:
+            data = entries_cache_json[feed_url]
+            if 'title' in data:
+              data.pop('title')
+            data['name'] = feed_name
 
         sortkey = feed_url.split('://')[-1]
         if sortkey.startswith('www.'):
@@ -42,5 +40,5 @@ with entries_cache.open('w', encoding='utf-8') as f:
 
 with feed_list.open('w', encoding='utf-8') as f:
     for _, feed_name, feed_url in feeds:
-        f.write(feed_name + '\n')
-        f.write(feed_url + '\n')
+        f.write(f'# {feed_name}\n')
+        f.write(f'{feed_url}\n')
